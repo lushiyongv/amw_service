43cbf248312ec5d3b6a271f1d039935f4e183e04
li.jinxiu@innobri.com <li.jinxiu@brixd.com>
2014-03-10 13:45:23 +0800

    first commit

---
+++ amway_service/amway_service/urls.py
@@ -0,0 +1,31 @@
+# -*- coding:utf-8 -*-
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from settings import STATIC_ROOT
+
+admin.autodiscover()
+# admin.site.unregister(Site)
+urlpatterns = patterns('',
+    url(r'^$', 'amway_service.views.index'),
+    url(r'^qyh$', 'qingyinghui_invitation.views.index'),
+    # url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT,
+    #                                                'show_indexes': True}),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': STATIC_ROOT,
+                                                   'show_indexes': True}),
+    url(r'^myinvitation/$', 'qingyinghui_invitation.views.myinvitation'),
+    url(r'^login/$', 'qingyinghui_invitation.views.login'),
+    url(r'^qyh_schedule/$', 'qingyinghui_invitation.views.schedule'),
+    url(r'^qyh/(\w+).html$', 'qingyinghui_invitation.views.show_qyh_html'),
+
+    # Examples:
+    # url(r'^$', 'qingyinghui_invitation.views.home', name='home'),
+    # url(r'^qingyinghui_invitation/', include('qingyinghui_invitation.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
+
+print 'settings.STATIC_ROOT', STATIC_ROOT
---
+++ amway_service/qingyinghui_invitation/views.py
@@ -0,0 +1,65 @@
+# -*- coding:utf-8 -*-
+# Create your views here.
+import urllib
+from django.core.exceptions import ObjectDoesNotExist
+
+from django.template import RequestContext
+from django.http import HttpResponse, HttpResponsePermanentRedirect, Http404
+from django.core.paginator import PageNotAnInteger, Paginator, InvalidPage, EmptyPage
+from django.views.decorators.csrf import csrf_exempt, ensure_csrf_cookie
+from django.shortcuts import render_to_response
+import json
+from datetime import datetime
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+def index(request):
+    return render_to_response('index.html', locals(), context_instance=RequestContext(request))
+
+def myinvitation(request):
+    return render_to_response('myinvitation.html', locals(), context_instance=RequestContext(request))
+
+def schedule(request):
+    return render_to_response('calendar.html', locals(), context_instance=RequestContext(request))
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+@csrf_exempt
+def login(request):
+    #TODO 记录 ip 时间   卡号  id  到文件中， 便于日后统计。
+    try:
+        ip = get_client_ip(request)
+        log_filehandler = open('./user_login_log/invitation_user_login.' + datetime.now().strftime('%Y-%m-%d') + '.log', 'a')
+        user_no = request.POST['ada']
+        user_name = request.POST['name']
+        user_login_log = '%s\t%s\t%s\t%s%s' % (ip, datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                                               str(user_no), user_name, '\n')
+        # print user_login_log
+        log_filehandler.write('%s' % user_login_log)
+    except:
+        print 'error:', request
+        print sys.exc_info()[0]
+    finally:
+        log_filehandler.close()
+
+
+    response_data = {}
+    response_data['result'] = 1
+
+    user_info = {}
+    user_info['ada'] = user_no
+    user_info['name'] = user_name
+    response_data['data'] = user_info
+    return HttpResponse(json.dumps(response_data), content_type="application/json")
+
+def show_qyh_html(request, html_page):
+    template = "%s.html" % html_page
+    return render_to_response(template, context_instance=RequestContext(request))
