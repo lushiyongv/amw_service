882a9944e8945ce2facfaa77a39b1f60cbbb9bde
li.jinxiu@innobri.com <li.jinxiu@brixd.com>
2014-03-15 20:13:34 +0800

    修改前端细节

--- amway_service/settings.py
+++ amway_service/settings.py
@@ -29,8 +29,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'amway_qingyinghui',                      # Or path to database file if using sqlite3.
-        'USER': 'cms',                      # Not used with sqlite3.
-        'PASSWORD': '211127',                  # Not used with sqlite3.
+        'USER': 'root',                      # Not used with sqlite3.
+        'PASSWORD': 'root',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
         'CONN_MAX_AGE': None,
