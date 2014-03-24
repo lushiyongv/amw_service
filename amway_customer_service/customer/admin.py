624dfc0dca38a529b4f614f692b1cbcfbd6eb56d
yujingye <yu.jingye@brixd.com>
2014-03-24 11:57:35 +0800

    remove customer service in this project

--- amway_customer_service/customer/admin.py
+++
@@ -1,5 +0,0 @@
-from amway_customer_service.customer.models import CustomerInfo, CustomerInfoHistory
-from django.contrib import admin
-
-admin.site.register(CustomerInfo)
-admin.site.register(CustomerInfoHistory)
--- amway_customer_service/sr/admin.py
+++
@@ -1,6 +0,0 @@
-from amway_customer_service.sr.models import SRFollowUpType, SRPermissionControl, SRUserInfo
-from django.contrib import admin
-
-admin.site.register(SRUserInfo)
-admin.site.register(SRFollowUpType)
-admin.site.register(SRPermissionControl)
