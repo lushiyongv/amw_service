9848c2b33b7d7da11c0fcf36fceb586aa6334795
lushiyong <lu.shiyong@brixd.com>
2014-04-26 08:49:57 +0800

    关闭debug模式

--- amway_service/settings.py
+++ amway_service/settings.py
@@ -4,7 +4,7 @@ import os
 from os.path import join, dirname
 PROJECT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))#当前路径的父目录
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -39,7 +39,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
